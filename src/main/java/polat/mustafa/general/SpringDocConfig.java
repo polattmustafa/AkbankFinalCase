05664735b064ce9f49d675985d61d5a3d452be28
Polat <mustfplt4@gmail.com>
2023-05-29 20:13:14 +0300

    added RestResonse & swagger

---
+++ src/main/java/polat/mustafa/general/SpringDocConfig.java
@@ -0,0 +1,22 @@
+package polat.mustafa.general;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MPolat
+ */
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI OpenApiBase() {
+        return new OpenAPI().info(new Info().title("Akbank Final Case"));
+    }
+
+}
