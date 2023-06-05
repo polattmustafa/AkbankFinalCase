60a6a5e3f1c7622244a7d58f4b1fb3657f8c5b55
Polat <mustfplt4@gmail.com>
2023-06-05 20:00:54 +0300

    weather controller added

--- src/main/java/polat/mustafa/controller/WeatherController.java
+++ src/main/java/polat/mustafa/controller/WeatherController.java
@@ -1,9 +1,9 @@
 package polat.mustafa.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import polat.mustafa.service.WeatherService;
+import org.springframework.web.bind.annotation.*;
+import polat.mustafa.entity.WeatherApiResponse;
+import polat.mustafa.service.WeatherApiClient;
 
 /**
  * @author MPolat
@@ -14,8 +14,12 @@ import polat.mustafa.service.WeatherService;
 @RequiredArgsConstructor
 public class WeatherController {
 
-    private final WeatherService weatherService;
+    private final WeatherApiClient weatherApiClient;
 
-
+    @GetMapping("/{city}")
+    public WeatherApiResponse getWeatherForecast(@PathVariable String city) {
+        WeatherApiResponse weatherApiResponse = weatherApiClient.getWeatherForecast(city);
+        return weatherApiResponse;
+    }
 
 }
