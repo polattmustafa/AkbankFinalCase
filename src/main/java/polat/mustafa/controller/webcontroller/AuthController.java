db1f314f773e49c56e2a5a899b19117f898fa303
Polat <mustfplt4@gmail.com>
2023-06-05 20:04:38 +0300

    added web auth controller

---
+++ src/main/java/polat/mustafa/controller/webcontroller/AuthController.java
@@ -0,0 +1,72 @@
+package polat.mustafa.controller.webcontroller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import polat.mustafa.dto.UserDtoWeb;
+import polat.mustafa.dto.user.UserDTO;
+import polat.mustafa.model.User;
+import polat.mustafa.service.UserServiceWeb;
+
+import java.util.List;
+
+/**
+ * @author MPolat
+ */
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserServiceWeb userServiceWeb;
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        // create model object to store form data
+        UserDtoWeb user = new UserDtoWeb();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDtoWeb userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userServiceWeb.findByEmail(userDto.getEmail());
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userServiceWeb.saveUser(userDto);
+        return "redirect:/register?success";
+    }
+
+    @GetMapping("/weather")
+    public String weather(){
+        return "weather";
+    }
+
+}
