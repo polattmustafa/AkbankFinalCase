db8b830534bad808cc609c692814bf18cc895f7a
Polat <mustfplt4@gmail.com>
2023-06-05 20:04:03 +0300

    spring security added

---
+++ src/main/java/polat/mustafa/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package polat.mustafa.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import polat.mustafa.model.User;
+import polat.mustafa.repository.UserRepository;
+
+import java.util.ArrayList;
+
+/**
+ * @author MPolat
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(),
+                    new ArrayList<>());
+        }else{
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+}
