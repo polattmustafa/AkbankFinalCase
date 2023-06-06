012337e35b4a495db5a4cc3371e2feb6755a8d58
Polat <mustfplt4@gmail.com>
2023-06-06 15:30:23 +0300

    rest template updates

--- src/main/java/polat/mustafa/service/RestTemplateConfig.java
+++ src/main/java/polat/mustafa/service/RestTemplateConfig.java
@@ -2,7 +2,11 @@ package polat.mustafa.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author MPolat
@@ -13,7 +17,15 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
+                jsonConverter.getObjectMapper().findAndRegisterModules();
+            }
+        }
+        return restTemplate;
     }
 
 }
